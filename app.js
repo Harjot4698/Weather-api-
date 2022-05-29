c4a13053b574eff9477625c8bdff095bf7c160a4
harjot <harjot23062000@gmail.com>
2022-05-29 02:24:39 -0700

    done

--- app.js
+++ app.js
@@ -3,8 +3,32 @@ const PORT=8000
 const express=require('express')
 const cheerio=require('cheerio')
 const axios=require('axios')
-
+const articles=[]
 
 const app=express()
 
-app.listen(PORT,()=>console.log(`server running on port${PORT}`))
+
+app.get('/',(req,res)=>{
+    res.json('Welcome to my Climate change News API')
+})
+
+app.get('/news',(req,res)=>{
+
+    axios.get('https://www.theguardian.com/world/extreme-weather')
+    .then((response) =>{
+        const html=response.data
+        console.log(html)
+        const $=cheerio.load(html)
+        $('a:contains("climate")',html).each(function(){
+           const title= $(this).text()
+           const url=$(this).attr('href')
+           articles.push({title,
+                 
+            url})
+        })
+        res.json(articles)
+    }).catch((err)=>console.log(err))
+
+})
+
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`))
