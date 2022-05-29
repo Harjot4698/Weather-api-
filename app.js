6c9cdb288e15eea0d3b1c63d07f543f5161397c8
harjot <harjot23062000@gmail.com>
2022-05-28 23:58:42 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const PORT=8000
+
+const express=require('express')
+const cheerio=require('cheerio')
+const axios=require('axios')
+
+
+const app=express()
+
+app.listen(PORT,()=>console.log(`server running on port${PORT}`))
